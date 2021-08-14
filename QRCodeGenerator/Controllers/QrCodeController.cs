1589e7f3d06c4400a7d9a4ecfb45bb502c9fe7b5
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-08-14 10:55:08 -0300

    Adicionar arquivos de projeto.

---
+++ QRCodeGenerator/Controllers/QrCodeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoderWeb.Services;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace QRCoderWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrCodeController : ControllerBase
+    { 
+        [HttpGet]
+        public IActionResult GenereteByteArray(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return BadRequest();
+
+            return Ok(QrCodeGenerator.GenerateByteArray(url));
+        }
+
+        [HttpGet("img")]
+        public HttpResponseMessage GetImage(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            var img = QrCodeGenerator.GenereteImage(url);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+                HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+                result.Content = new ByteArrayContent(ms.ToArray());
+                result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+
+                return result;
+            }
+        }
+    }
+}
---
+++ QRCodeGenerator/Services/QrCodeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.IO;
+using QRCoder;
+
+namespace QRCoderWeb.Services
+{
+    public static class QrCodeGenerator
+    {
+        public static Bitmap GenereteImage(string url)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(10);
+
+            return qrCodeImage;
+        }
+
+        public static byte[] GenerateByteArray(string url)
+        {
+            var image = GenereteImage(url);
+            return ImageToByte(image);
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            using var stream = new MemoryStream();
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+    }
+}
